c00085232a14afd865693b382230db63f8fb3924
Himanshi <himanshipal74@gmail.com>
2026-02-24 20:10:38 +0530

    upfated model

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
 from google import genai
-client = genai.Client(api_key="AIzaSyCj8xyFZY1MEMIHAIAJjZ3_DhaD3pOS26o")
+client = genai.Client(api_key="AIzaSyBrhb_1Et5G-Yd7z1UBv54q9lyWvcr8bLg")
 for model in client.models.list():
     print(model.name)
